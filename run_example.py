2d24a6678a61890955aa42b6b36d6bbbb0a484a7
Francisco José Mata Aroco <tbd>
2022-04-20 19:54:35 +0200

    minor update

--- run_example.py
+++ run_example.py
@@ -8,7 +8,7 @@ from kmaintainer import FileMaintainer
 
 if __name__ == "__main__":
     this_file_path = Path(__file__).parent.resolve()
-    logger = logtools.get_fast_logger("RawArranger", this_file_path)
+    logger = logtools.get_fast_logger("FileMaintainer", this_file_path)
     folder_patterns = ("1.*", "2.*", "3.*", "4.*", "5.*", )
     year_bounds = (1800, 2300)
     newest_only = True
